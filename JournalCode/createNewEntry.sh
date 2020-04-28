058c3219aa20dd314bce8ffe123735d33dddb0e7
Aaron Oldenburg <aoldenburg@ubalt.edu>
2020-04-28 06:49:07 -0400

    more functions

--- JournalCode/createNewEntry.sh
+++ JournalCode/createNewEntry.sh
@@ -5,38 +5,70 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-DATE=`date +%Y-%m-%d`
-nameOfFile="../Journal/"$DATE".txt"
+checkInterval=3600 # seconds
 
-printf "Looking to see if today's entry already exists.\n\n"
+new_file_name() {
+    DATE=`date +%Y-%m-%d`
+    file="../Journal/"$DATE".txt"
+    echo $file
+}
 
-if [[ -f $nameOfFile ]]; then
-    printf $nameOfFile" exists already.\n\n"
-    exit
-fi
+check_file_exists() {
+    printf "Looking to see if today's entry already exists.\n\n"
 
-printf "It doesn't exist. Creating it.\n\n"
+    if [[ -f $nameOfFile ]]; then
+        printf $nameOfFile" exists already.\n\n"
+        echo true
+    else
+        printf "It doesn't exist. Creating it.\n\n"
+        echo false
+    fi
+}
+
+get_random_prompt() {
+    prompts=("What did you notice today?"
+            "What do you see outside your window?"
+            "Describe a forest."
+            "Where are you?"
+            "Who or what has passed through your home recently?"
+            "What have you dreamed about, or are you dreaming about?"
+            "What does the air feel like?"
+            "Describe a meal."
+            "Pick a location in your body Describe in detail how it feels.")
+    len=${#prompts[@]}
+    rand=$(($RANDOM%$len))
+    prompt=${prompts[rand]}
+    echo $prompt
+}
+
+create_file() {
+    todayPrompt=$(get_random_prompt)
+
+    day=$(date +%A)
+    month=$(date +%B)
+    date=$(date +%d)
+    year=$(date +%Y)
+    time=$(date +"%r")
+
+    printf $day", "$month" "$date", "$year"\n"$time"\n\n" > $nameOfFile
+    printf $todayPrompt"\n\n" >> $nameOfFile
+
+        
+    # adding +num after nano puts the cursor at that line
+    # or whatever last line is
+    nano +666 $nameOfFile
+}
+
+while true
+do
+    nameOfFile=$(new_file_name)
+    if [$(check_file_exists)="true"]; then
+        exit
+    fi
+    create_file
+    sleep $checkInterval
+done
 
 
-# Create random prompt
-# Load from json file
 
-prompts=("What did you notice today?"
-        "What do you see outside your window?"
-        "Describe a forest."
-        "Where are you?")
-len=${#prompts[@]}
-rand=$(($RANDOM%$len))
-todayPrompt=${prompts[rand]}
-day=$(date +%A)
-month=$(date +%B)
-date=$(date +%d)
-year=$(date +%Y)
-time=$(date +"%r")
 
-printf $day", "$month" "$date", "$year"\n"$time"\n\n" > $nameOfFile
-printf $todayPrompt"\n\n" >> $nameOfFile
-
-# adding +num after nano puts the cursor at that line
-# or whatever last line is
-nano +666 $nameOfFile
--- JournalCode/mystartup.sh
+++ JournalCode/mystartup.sh
@@ -4,7 +4,12 @@
 set -euo pipefail
 IFS=$'\n\t'
 
+waitInterval=30
+
+sleep $waitInterval
+
 echo "Beginning my scripts."
 
 # For now hold off, since can't see the results
-# sh '/home/aarono/CurrentProject/JournalCode/checkfolder.sh' &
+sh checkfolder.sh &
+sh createNewEntry.sh &

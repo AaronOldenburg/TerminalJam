ea3de87e9e3280094c3ed8735909db8566181d59
Aaron Oldenburg <aoldenburg@ubalt.edu>
2020-04-30 06:43:39 -0400

    stats functioning

--- JournalCode/dostats.sh
+++ JournalCode/dostats.sh
@@ -7,15 +7,26 @@ IFS=$'\n\t'
 filename="../stats.txt"
 DATE=`date +%c`
 health=100
+MAX_HIT=50
 
 printf "Checking file...\n\n"
 
 update_file() {
+    range_double=$(( MAX_HIT*2 ))
+    hit_amt=$(( $((RANDOM%range_double))-MAX_HIT ))
+    printf "\n"$hit_amt"\n"
+
     printf "\n\nOn "$DATE", " >> $filename
     
-    printf "you lost 20 health.\n" >> $filename
-    
-    health=$((health-20))
+    if [ $hit_amt -gt 0 ]; then
+        printf "you gained "$hit_amt" health.\n" >> $filename
+    elif [ $hit_amt -lt 0 ]; then
+        printf "you lost "$hit_amt" health.\n" >> $filename
+    else
+        printf "your health stayed the same.\n" >> $filename
+    fi
+        
+    health=$((health+hit_amt))
     
     if [ $health -gt 0 ]; then
         printf "You are alive.\n" >> $filename
@@ -32,7 +43,7 @@ if [[ -f $filename ]]; then
     update_file
 else
     printf "New file\n"
-    defaultText="For those of you who feel this isn't enough of a game:\n\n"
+    defaultText="Your game stats.\n\n"
     printf $defaultText > $filename
     update_file
 fi
